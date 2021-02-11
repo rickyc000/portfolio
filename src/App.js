006db1dc3b57c2dc73c447c3cf989aa5b4080ca9
rickyc <r.cato@live.com>
2021-02-11 20:22:08 +0000

    sass added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react' 
+import React from 'react'
+import './styles/main.scss'
 
 function App() {
-  const message = 'Ricky Cato Portfolio'
-  
+  // const message = 'Ricky Cato'
+
   return (
-    <h1>{message}</h1>
+    <main>
+      <h1 className="test">Ricky Cato</h1>
+      <div>
+        Site under construction
+      </div>
+      <div>
+        <a href="https://github.com/rickyc000">GitHub</a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/ricky-cato/">LinkedIn</a>
+      </div>
+      <div>
+        <a href="mailto:r.cato@live.com">Email</a>
+      </div>
+    </main>
   )
 }
 
 
 export default App 
+
