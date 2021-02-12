256177078a4aaa2dd44ef94e23f09d8278654db7
rickyc <r.cato@live.com>
2021-02-12 10:10:24 +0000

    basic info and styling

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ function App() {
   return (
     <main>
       <h1 className="test">Ricky Cato</h1>
-      <div>
+      <div className="links">
+        <div>
+          <a href="https://github.com/rickyc000">GitHub</a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/ricky-cato/">LinkedIn</a>
+        </div>
+        <div>
+          <a href="mailto:r.cato@live.com">Email</a>
+        </div>
+      </div>
+      <div className="bio">
         Site under construction
       </div>
-      <div>
-        <a href="https://github.com/rickyc000">GitHub</a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/ricky-cato/">LinkedIn</a>
-      </div>
-      <div>
-        <a href="mailto:r.cato@live.com">Email</a>
-      </div>
+
     </main>
   )
 }
 
 
-export default App 
+export default App
 
